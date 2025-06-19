846a1be675a536e3d91f096184faac4a16e79ee9
xiebaiyuan <xiebaiyuan@139.com>
2025-06-19 14:10:20 +0800

    windows x64 build success

--- build_windows.bat
+++ build_windows.bat
@@ -42,10 +42,105 @@ REM Build the project
 echo Building project...
 cmake --build . --config Release
 
-REM Copy DLL to output directory
-echo Copying DLLs to output...
+REM Copy additional DLLs that might be needed at runtime
+echo Copying additional runtime dependencies...
 if not exist Release mkdir Release
-copy D:\workspace\paddle_inference_win_x86_mkl\paddle\lib\paddle_inference.dll Release\ /Y
 
+REM Set base paths for convenience
+set PADDLE_BASE=D:\workspace\paddle_inference_win_x86_mkl
+set THIRD_PARTY=%PADDLE_BASE%\third_party\install
+
+REM Copy Paddle main DLL
+echo Checking for Paddle main DLL...
+if exist "%PADDLE_BASE%\paddle\lib\paddle_inference.dll" (
+    echo   - Copying paddle_inference.dll
+    copy "%PADDLE_BASE%\paddle\lib\paddle_inference.dll" Release\ /Y
+) else (
+    echo   - WARNING: paddle_inference.dll not found
+)
+
+if exist "%PADDLE_BASE%\paddle\lib\common.dll" (
+    echo   - Copyingcommon.dll
+    copy "%PADDLE_BASE%\paddle\lib\common.dll" Release\ /Y
+) else (
+    echo   - WARNING: common.dll not found
+)
+
+REM Copy MKL-ML dependencies
+echo Checking for MKL-ML dependencies...
+if exist "%THIRD_PARTY%\mklml\lib\mklml.dll" (
+    echo   - Copying mklml.dll
+    copy "%THIRD_PARTY%\mklml\lib\mklml.dll" Release\ /Y
+) else (
+    echo   - WARNING: mklml.dll not found
+)
+
+if exist "%THIRD_PARTY%\mklml\lib\libiomp5md.dll" (
+    echo   - Copying libiomp5md.dll
+    copy "%THIRD_PARTY%\mklml\lib\libiomp5md.dll" Release\ /Y
+) else (
+    echo   - WARNING: libiomp5md.dll not found
+)
+
+REM Copy MKLDNN (OneDNN) dependencies
+echo Checking for MKLDNN dependencies...
+if exist "%THIRD_PARTY%\mkldnn\lib\mkldnn.dll" (
+    echo   - Copying mkldnn.dll
+    copy "%THIRD_PARTY%\mkldnn\lib\mkldnn.dll" Release\ /Y
+) else if exist "%THIRD_PARTY%\mkldnn\lib\dnnl.dll" (
+    echo   - Copying dnnl.dll (newer OneDNN name)
+    copy "%THIRD_PARTY%\mkldnn\lib\dnnl.dll" Release\ /Y
+) else (
+    echo   - WARNING: mkldnn.dll/dnnl.dll not found
+)
+
+REM Copy other potential dependencies
+echo Checking for other dependencies...
+
+REM protobuf
+if exist "%THIRD_PARTY%\protobuf\lib\libprotobuf.dll" (
+    echo   - Copying libprotobuf.dll
+    copy "%THIRD_PARTY%\protobuf\lib\libprotobuf.dll" Release\ /Y
+)
+
+REM glog
+if exist "%THIRD_PARTY%\glog\lib\glog.dll" (
+    echo   - Copying glog.dll
+    copy "%THIRD_PARTY%\glog\lib\glog.dll" Release\ /Y
+)
+
+REM gflags
+if exist "%THIRD_PARTY%\gflags\lib\gflags.dll" (
+    echo   - Copying gflags.dll
+    copy "%THIRD_PARTY%\gflags\lib\gflags.dll" Release\ /Y
+)
+
+REM xxhash
+if exist "%THIRD_PARTY%\xxhash\lib\xxhash.dll" (
+    echo   - Copying xxhash.dll
+    copy "%THIRD_PARTY%\xxhash\lib\xxhash.dll" Release\ /Y
+)
+
+REM Check for additional common locations
+echo Checking alternative locations for mkldnn.dll...
+for %%p in (
+    "%PADDLE_BASE%\paddle\lib\mkldnn.dll"
+    "%PADDLE_BASE%\lib\mkldnn.dll"
+    "%THIRD_PARTY%\onednn\lib\dnnl.dll"
+    "%THIRD_PARTY%\onednn\lib\mkldnn.dll"
+) do (
+    if exist "%%p" (
+        echo   - Found and copying %%p
+        copy "%%p" Release\ /Y
+    )
+)
+
+REM List what was actually copied
+echo.
+echo Files in Release directory:
+dir Release\*.dll
+
+echo.
+echo Runtime dependencies copied successfully.
 echo Build completed.
 cd ..
